7274a5020837d57b35328adf3d4761587567923d
Akshayrsonar <rsonarakshay16@gmail.com>
2023-11-08 12:14:55 +0530

    'gavhj'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Loginpage from './components/Loginpage/Loginpage';
+
+function App() {
+  return (
+    <div >
+      <Loginpage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Loginpage/Loginpage.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./loginpage.css";
+
+import user_icon from "../assets/person.png";
+import email_icon from "../assets/email.png";
+import password_icon from "../assets/password.png";
+
+const Loginpage = () => {
+
+    const[action,setAction] =useState ("login")
+  return (
+  
+   <div className="container">
+        <div className="Header">
+            <div className="text">{action}</div>
+            <div className="underline"></div>
+        </div>
+        
+        <div className="inputs">
+        {action==="Login"?<div></div>:<div className="input">
+              
+              <img src={user_icon} alt="" />
+              <input type="text" placeholder="Name"/>
+          </div>}
+            
+
+            <div className="input">
+                <img src= {email_icon} alt="" />
+                <input type="email" placeholder="Email" />
+            </div>
+
+            <div className="input">
+                <img src= {password_icon} alt="" />
+                <input type="password" placeholder="Password" />
+            </div>
+        </div>
+        {action==="Sign Up"?<div></div>: <div className="forgot-password">lost password? <span>clickhere</span></div> }
+           
+        <div className="submit-container">
+            <div className={action==="Login"?"submit gray":"submit"} onClick ={()=>{setAction("Sign Up")}}>Signup</div>
+            <div className={action==="Sign up"?"submit gray":"submit"}onClick ={()=>{setAction("Login")}}>Login</div>
+        </div>
+    </div>
+  );
+};
+
+export default Loginpage;
